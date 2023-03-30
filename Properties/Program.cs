e520a3c24490a44570a2798300e8d0cb0ab41119
Asim-hameed <asimhameed31@gmail.com>
2023-03-30 22:50:03 +0500

    .Read/Write .ReadOnly .WriteOnly (Set and Get method to access private member variables)

--- Properties/Program.cs
+++ Properties/Program.cs
@@ -5,7 +5,12 @@
         private int _stdId;
         private string _fName;
         private string _lName;
+        private int _subTotal = 100;
 
+        public int subTotal
+        {
+            get { return this._subTotal; }
+        }
         public int stdId
         {
             set 
@@ -52,19 +57,28 @@
             get { return this._lName; }
 
         }
+        public int CNIC { get; private set; }
+        public student(string fname,string lname,int cnic)
+        {
+            fname = fname.ToLower();
+            lname = lname.ToLower();
+            CNIC = cnic;
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
             // Console.WriteLine("Hello, World!");
-            student student = new student();
+            student student = new student("Ali","Hameed",32304);
+            Console.WriteLine(student.CNIC);
             student.stdId = 0365;
             student.fName = "Asim";
             student.lName = "Hameed";
             Console.WriteLine( student.stdId);
             Console.WriteLine("Your name is {0}",student.fName);
-            Console.WriteLine("Your name is {0}", student.lName);
+            Console.WriteLine("Your Father name is {0}", student.lName);
+            Console.WriteLine("Total marks are {0}", student.subTotal);
 
         }
     }
