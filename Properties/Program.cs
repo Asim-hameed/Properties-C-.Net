a5bbc49700d0aa65f4655e79397d533f6eae0350
Asim-hameed <asimhameed31@gmail.com>
2023-03-30 22:30:38 +0500

    Basic Property example

--- Properties/Program.cs
+++ Properties/Program.cs
@@ -1,10 +1,71 @@
 ﻿namespace Properties
 {
-    internal class Program
+    class student
+    {
+        private int _stdId;
+        private string _fName;
+        private string _lName;
+
+        public int stdId
+        {
+            set 
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Id cannot be zero or negative");
+                }
+                else
+                {
+                    _stdId = value;
+                }
+            }
+            get { return this._stdId; }
+        }
+        public string fName
+        {
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Please enter your name: ");
+                }
+                else
+                {
+                    _fName = value;
+                }
+            }
+            get { return this._fName; }
+        }
+        public string lName
+        {
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Please enter your name: ");
+                }
+                else
+                {
+                    _lName = value;
+                }
+            }
+            get { return this._lName; }
+
+        }
+    }
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Console.WriteLine("Hello, World!");
+            student student = new student();
+            student.stdId = 0365;
+            student.fName = "Asim";
+            student.lName = "Hameed";
+            Console.WriteLine( student.stdId);
+            Console.WriteLine("Your name is {0}",student.fName);
+            Console.WriteLine("Your name is {0}", student.lName);
+
         }
     }
 }
